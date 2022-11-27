a7c83e5a5793e8c06e1f49837e05267740f21bdb
Damian Poddebniak <poddebniak@mailbox.org>
2022-11-28 00:08:38 +0100

    tests: Add Rust tests.

---
+++ tests/rust/test_rust.py
@@ -0,0 +1,25 @@
+import os
+
+from tree_sitter_parser import EMPTY_CONFIG, init_parsers, parse_and_translate
+
+
+def test_string_literal_differences():
+    script_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__))) + "/../.."
+    parsers = init_parsers(script_dir)
+
+    left = b"""
+    fn main() {
+        let msg = "A";
+    }"""
+
+    right = b"""
+    fn main() {
+        let msg = "B";
+    }"""
+
+    config = {"flattened": ["string_literal"], "aliased": {}, "ignored": []}
+
+    got_left = parse_and_translate(parsers["rust"], config, left)
+    got_right = parse_and_translate(parsers["rust"], config, right)
+
+    assert got_left != got_right, "Trees should have been different."
