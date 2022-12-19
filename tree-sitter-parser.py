5d9185fb6c1f510610b198d7d8968e67316533d5
Damian Poddebniak <poddebniak@mailbox.org>
2022-12-19 10:25:57 +0100

    refactor: Refactor (and extract) a `tree_sitter_parser` module. (#8)
    * refactor: Refactor (and extract) a `tree_sitter_parser` module.
    
    * tests: Introduce `pytest`.
    
    * tests: Add Rust tests.

--- tree-sitter-parser.py
+++ tree-sitter-parser.py
@@ -1,129 +1,57 @@
 #!/usr/bin/env python3
 
-from tree_sitter import Language, Parser
-from xml.dom import minidom
-from io import StringIO
-import os
 import argparse
+import os
+from io import StringIO
+
 import yaml
-
-script_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-rules_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__))) + "/rules.yml"
-
-EMPTY_CONFIG = {'flattened': [], 'aliased': {}, 'ignored': []}
-
-MAX_LABEL_SIZE = 75
-
-with open(rules_file, "r") as stream:
-  TREE_REWRITE_RULES = yaml.safe_load(stream)
-
-Language.build_library(
-  script_dir + '/build/languages.so',
-  [
-    script_dir + '/tree-sitter-c',
-    script_dir + '/tree-sitter-c-sharp',
-    script_dir + '/tree-sitter-java',
-    script_dir + '/tree-sitter-javascript',
-    script_dir + '/tree-sitter-ocaml/ocaml',
-    script_dir + '/tree-sitter-php',
-    script_dir + '/tree-sitter-python',
-    script_dir + '/tree-sitter-r',
-    script_dir + '/tree-sitter-ruby',
-    script_dir + '/tree-sitter-rust',
-    script_dir + '/tree-sitter-typescript/typescript'
-  ]
+from tree_sitter_parser import (
+    EMPTY_CONFIG,
+    init_parsers,
+    parse_and_translate,
+    pretty_print_ast,
 )
 
-PARSERS = {
-  "c": Language(script_dir + '/build/languages.so', 'c'),
-  "csharp": Language(script_dir + '/build/languages.so', 'c_sharp'),
-  "java": Language(script_dir + '/build/languages.so', 'java'),
-  "javascript": Language(script_dir + '/build/languages.so', 'javascript'),
-  "ocaml": Language(script_dir + '/build/languages.so', 'ocaml'),
-  "php": Language(script_dir + '/build/languages.so', 'php'),
-  "python": Language(script_dir + '/build/languages.so', 'python'),
-  "r": Language(script_dir + '/build/languages.so', 'r'),
-  "ruby": Language(script_dir + '/build/languages.so', 'ruby'),
-  "rust": Language(script_dir + '/build/languages.so', 'rust'),
-  "typescript": Language(script_dir + '/build/languages.so', 'typescript'),
-}
 
-positions = [0]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="path to the file to parse")
+    parser.add_argument("language", help="language of to the file to parse")
+    parser.add_argument(
+        "--raw", action="store_true", help="deactivate the rewrite rules"
+    )
+    parser.add_argument(
+        "--pretty",
+        action="store_true",
+        help="pretty print the AST instead of using XML",
+    )
+    args = parser.parse_args()
 
-doc = minidom.Document()
+    script_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+    rules_file = (
+        os.path.abspath(os.path.dirname(os.path.realpath(__file__))) + "/rules.yml"
+    )
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file", help="path to the file to parse")
-parser.add_argument("language", help="language of to the file to parse")
-parser.add_argument("--raw", action="store_true", help="deactivate the rewrite rules")
-parser.add_argument("--pretty", action="store_true", help="pretty print the AST instead of using XML")
-args = parser.parse_args()
+    with open(rules_file, "r") as stream:
+        TREE_REWRITE_RULES = yaml.safe_load(stream)
+    config = (
+        TREE_REWRITE_RULES[args.language]
+        if not args.raw and args.language in TREE_REWRITE_RULES
+        else EMPTY_CONFIG
+    )
 
-def main(file, language):
-  parser = Parser()
-  parser.set_language(PARSERS[language])
-  config = retrieveConfig(language)
-  tree = parser.parse(bytes(readFile(file), "utf8"))
-  xmlRoot = toXmlNode(tree.root_node, config)
-  doc.appendChild(xmlRoot)
-  process(tree.root_node, xmlRoot, config)
-  if args.pretty:
-    out = StringIO()
-    pretty_print_ast(doc.firstChild, out)
-    print(out.getvalue())
-  else:
-    print(doc.toprettyxml())
+    parsers = init_parsers(script_dir)
+    doc = parse_and_translate(
+        parsers[args.language], config, open(args.file, "rb").read()
+    )
 
-def pretty_print_ast(elm, out, level=0):
-  elm_desc = f'\033[1m{elm.getAttribute("type")}\033[0m'
-  if elm.hasAttribute("label"):
-    elm_desc += f' \033[94m{sanitize_label(elm.getAttribute("label"))}\033[0m'
-  left_bound = int(elm.getAttribute("pos"))
-  right_bound = left_bound + int(elm.getAttribute("length"))
-  elm_desc += f' [{str(left_bound)},{right_bound}]'
-  log_start = "" if level == 0 else "\n"
-  out.write(f'{log_start}{"  " * level}{elm_desc}')
-  for child in elm.childNodes:
-    pretty_print_ast(child, out, level + 1)
+    if args.pretty:
+        out = StringIO()
+        pretty_print_ast(doc.firstChild, out)
+        print(out.getvalue())
+    else:
+        print(doc.toprettyxml())
 
-def sanitize_label(raw_label):
-  raw_label = raw_label.replace("\n", "")
-  raw_label = raw_label.replace("\t", "")
-  label = (raw_label[:MAX_LABEL_SIZE] + '..') if len(raw_label) > MAX_LABEL_SIZE else raw_label
-  return label
 
-def process(node, xmlNode, config):
-  if not node.type in config['flattened']:
-    for child in node.children:
-      if not child.type in config['ignored']:
-        xmlChildNode = toXmlNode(child, config)
-        xmlNode.appendChild(xmlChildNode)
-        process(child, xmlChildNode, config)
-
-def retrieveConfig(language):
-  return TREE_REWRITE_RULES[language] if not args.raw and language in TREE_REWRITE_RULES else EMPTY_CONFIG
-
-def toXmlNode(node, config):
-  xmlNode = doc.createElement('tree')
-  type = config['aliased'][node.type] if node.type in config['aliased'] else node.type
-  xmlNode.setAttribute("type", type)
-  startPos = positions[node.start_point[0]] + node.start_point[1]
-  endPos = positions[node.end_point[0]] + node.end_point[1]
-  length = endPos - startPos
-  xmlNode.setAttribute("pos", str(startPos))
-  xmlNode.setAttribute("length", str(length))
-  if node.child_count == 0 or node.type in config['flattened']:
-    xmlNode.setAttribute("label", node.text.decode('utf8'))
-  return xmlNode
-
-def readFile(file):
-  with open(file, 'r') as file:
-    data = file.read()
-  index = 0
-  for chr in data:
-    index += 1
-    if chr == '\n':
-      positions.append(index)
-  return data
-
-main(args.file, args.language)
+if __name__ == "__main__":
+    main()
---
+++ tree_sitter_parser/__init__.py
@@ -0,0 +1,136 @@
+import sys
+from xml.dom import minidom
+
+from tree_sitter import Language, Parser
+
+
+EMPTY_CONFIG = {"flattened": [], "aliased": {}, "ignored": []}
+MAX_LABEL_SIZE = 75
+
+
+def eprint(*args, **kwargs):
+    """
+    Same as `print` but writes to `sys.stderr`.
+    """
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def init_parsers(script_dir):
+    """
+    Compile parsers (when needed) and return a parser map that can be indexed by language.
+    """
+    if Language.build_library(
+        script_dir + "/build/languages.so",
+        [
+            script_dir + "/tree-sitter-c",
+            script_dir + "/tree-sitter-c-sharp",
+            script_dir + "/tree-sitter-java",
+            script_dir + "/tree-sitter-javascript",
+            script_dir + "/tree-sitter-ocaml/ocaml",
+            script_dir + "/tree-sitter-php",
+            script_dir + "/tree-sitter-python",
+            script_dir + "/tree-sitter-r",
+            script_dir + "/tree-sitter-ruby",
+            script_dir + "/tree-sitter-rust",
+            script_dir + "/tree-sitter-typescript/typescript",
+        ],
+    ):
+        eprint("Compiled dynamic library of parsers.")
+    else:
+        eprint("Reusing dynamic library of parsers.")
+
+    return {
+        "c": Language(script_dir + "/build/languages.so", "c"),
+        "csharp": Language(script_dir + "/build/languages.so", "c_sharp"),
+        "java": Language(script_dir + "/build/languages.so", "java"),
+        "javascript": Language(script_dir + "/build/languages.so", "javascript"),
+        "ocaml": Language(script_dir + "/build/languages.so", "ocaml"),
+        "php": Language(script_dir + "/build/languages.so", "php"),
+        "python": Language(script_dir + "/build/languages.so", "python"),
+        "r": Language(script_dir + "/build/languages.so", "r"),
+        "ruby": Language(script_dir + "/build/languages.so", "ruby"),
+        "rust": Language(script_dir + "/build/languages.so", "rust"),
+        "typescript": Language(script_dir + "/build/languages.so", "typescript"),
+    }
+
+
+def parse_and_translate(parser_lang, config, input: bytes):
+    """
+    Parse a file and translate the obtained AST to the GumTree XML format.
+    """
+    newline_offsets = create_newline_offsets(input)
+
+    parser = Parser()
+    parser.set_language(parser_lang)
+    tree = parser.parse(input)
+
+    doc = minidom.Document()
+    xml_root = to_xml_node(doc, tree.root_node, config, newline_offsets)
+    doc.appendChild(xml_root)
+    process(doc, tree.root_node, xml_root, config, newline_offsets)
+
+    return doc
+
+
+def create_newline_offsets(input: bytes):
+    """
+    Obtain a list of indices of all newlines in a file. The first line has offset 0.
+
+    This list can be used to translate from `(line, column)` to `pos` by using ...
+
+        pos = offsets[line] + column
+    """
+    offsets = [0]
+
+    for (i, chr) in enumerate(input.decode("utf-8"), start=1):
+        if chr == "\n":
+            offsets.append(i)
+
+    return offsets
+
+
+def process(doc, node, xml_node, config, newline_offsets):
+    if not node.type in config["flattened"]:
+        for child in node.children:
+            if not child.type in config["ignored"]:
+                xml_child_node = to_xml_node(doc, child, config, newline_offsets)
+                xml_node.appendChild(xml_child_node)
+                process(doc, child, xml_child_node, config, newline_offsets)
+
+
+def to_xml_node(doc, node, config, newline_offsets):
+    xmlNode = doc.createElement("tree")
+    type = config["aliased"][node.type] if node.type in config["aliased"] else node.type
+    xmlNode.setAttribute("type", type)
+    startPos = newline_offsets[node.start_point[0]] + node.start_point[1]
+    endPos = newline_offsets[node.end_point[0]] + node.end_point[1]
+    length = endPos - startPos
+    xmlNode.setAttribute("pos", str(startPos))
+    xmlNode.setAttribute("length", str(length))
+    if node.child_count == 0 or node.type in config["flattened"]:
+        xmlNode.setAttribute("label", node.text.decode("utf8"))
+    return xmlNode
+
+
+def pretty_print_ast(elm, out, level=0):
+    elm_desc = f'\033[1m{elm.getAttribute("type")}\033[0m'
+    if elm.hasAttribute("label"):
+        elm_desc += f' \033[94m{sanitize_label(elm.getAttribute("label"))}\033[0m'
+    left_bound = int(elm.getAttribute("pos"))
+    right_bound = left_bound + int(elm.getAttribute("length"))
+    elm_desc += f" [{str(left_bound)},{right_bound}]"
+    log_start = "" if level == 0 else "\n"
+    out.write(f'{log_start}{"  " * level}{elm_desc}')
+    for child in elm.childNodes:
+        pretty_print_ast(child, out, level + 1)
+
+
+def sanitize_label(raw_label):
+    raw_label = raw_label.replace("\n", "")
+    raw_label = raw_label.replace("\t", "")
+    label = (
+        (raw_label[:MAX_LABEL_SIZE] + "..")
+        if len(raw_label) > MAX_LABEL_SIZE
+        else raw_label
+    )
+    return label
